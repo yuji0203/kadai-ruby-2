62fdb50a73f9aafb0fa1cb97866e47c2057bf523
yuji <awkwwf0203@gmail.com>
2021-06-17 10:01:46 +0000

    fizzbizz2

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,16 +1,22 @@
 def fizzbuzz(num)
-    num_max = 100
-    (1..num_max).each do |num|
-        if num % 3 == 0 && num % 5 == 0
-        puts 'FizzBuzz'
-        elsif num % 3 == 0
-        puts 'Fizz'
-        elsif num % 5 == 0
-        puts 'Buzz'
-        else
-        puts num
-        end
+    if num % 3 == 0 && num % 5 == 0
+    'FizzBuzz'
+    elsif num % 3 == 0
+    'Fizz'
+    elsif num % 5 == 0
+    'Buzz'
+    else
+    num
     end
 end
-
-fizzbuzz(1)
+num_max = 100
+     (1..num_max).each do |number|
+    puts fizzbuzz(number)
+end
+# 以下は動作確認用の記述です
+=begin
+puts fizzbuzz(1)
+puts fizzbuzz(3)
+puts fizzbuzz(5)
+puts fizzbuzz(15)7
+=end
